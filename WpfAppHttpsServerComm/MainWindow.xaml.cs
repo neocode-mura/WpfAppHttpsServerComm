2b51368fc446f06d6ba07ab6dfebc56ccbb22200
m.murakami <m.murakami@neocode.jp>
2020-09-07 10:24:35 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfAppHttpsServerComm/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+
+namespace WpfAppHttpsServerComm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        HttpClient client;
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            client = new HttpClient() { Timeout = TimeSpan.FromSeconds(10) };
+            TextBoxResponse.Text = "";
+        }
+
+        private async void ButtonPost_Click(object sender, RoutedEventArgs e)
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                {"cmd", TextBoxPostData.Text }
+            };
+            TextBoxResponse.Text = "";
+            TextBoxStatus.Text = "";
+
+            using (var content = new FormUrlEncodedContent(parameters))
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync(TextBoxUrl.Text, content);
+                    TextBoxStatus.Text = response.ReasonPhrase;
+                    HttpContent responseContent = response.Content;
+                    using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+                    {
+                        TextBoxResponse.Text = await reader.ReadToEndAsync();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    TextBoxStatus.Text = ex.Message;
+                }
+            }
+        } 
+    }
+}
