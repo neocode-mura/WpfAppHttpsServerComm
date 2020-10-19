eaf05c730addd053c38a8e9ef65d2f9b6e65294a
m.murakami <m.murakami@neocode.jp>
2020-10-19 17:08:04 +0900

    v1.0.1 README.mdとLICENCEを追加

--- WpfAppHttpsServerComm/MainWindow.xaml.cs
+++ WpfAppHttpsServerComm/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net.Http;
 using System.Windows;
@@ -33,16 +35,21 @@ namespace WpfAppHttpsServerComm
             TextBoxResponse.Text = "";
             TextBoxStatus.Text = "";
 
+            var sw = new System.Diagnostics.Stopwatch();
             using (var content = new FormUrlEncodedContent(parameters))
             {
                 try
                 {
+                    sw.Start();
                     HttpResponseMessage response = await client.PostAsync(TextBoxUrl.Text, content);
                     TextBoxStatus.Text = response.ReasonPhrase;
                     HttpContent responseContent = response.Content;
                     using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
                     {
+                        sw.Stop();
                         TextBoxResponse.Text = await reader.ReadToEndAsync();
+                        TimeSpan ts = sw.Elapsed;
+                        TextBoxResponse.Text += " / " + ts.Seconds + "秒" + ts.Milliseconds + "m秒";
                     }
                 }
                 catch(Exception ex)
---
+++ WpfAppHttpsServerComm/README.md
@@ -0,0 +1,3 @@
+# HTTP Server通信 PC(WPF)アプリ
+
+HTTP Serverと通信するPC(WPF)アプリです。
